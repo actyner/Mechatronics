405e72ba8ed8842d9c0877a74a65b239faa0355f
actyner <49135868+actyner@users.noreply.github.com>
2019-05-08 22:52:42 -0500

    Create main.c

---
+++ HW11/main.c
@@ -0,0 +1,34 @@
+#include "i2c_master_noint.h"
+#include<stdio.h> //sprintf
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all MPLAB Harmony modules, including application(s). */
+    SYS_Initialize ( NULL );
+    
+
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
